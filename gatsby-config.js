75187a97eeadd7e1d7f62d0f2d1525f17e4d990f
huytrinh <dinhhuyams@gmail.com>
2021-08-28 18:49:13 +0300

    feat: add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,27 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Simple Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@huytrinh',
+    person: {name: 'john', age: 32},
+    simpleData: ['item1', 'item2'],
+    complexData: [{name: 'john', age: 32}, {name: 'susan', age: 23}]
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
     },
   `gatsby-plugin-image`,
   `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`
+  `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `project`,
+        path: `${__dirname}/src`,
+      },
+    },
   ],
 }
